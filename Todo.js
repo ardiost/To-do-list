ff07575685813a28d6ff92a70471d34dbb6cfba5
Ardios <mojtabaostad1379@gmail.com>
2023-09-27 15:05:22 +0330

    how to refactor file

---
+++ Todo.js
@@ -0,0 +1,20 @@
+import { data } from "./data";
+
+class Todo {
+  getItems() {
+    if 
+    return data;
+  }
+}
+
+
+(new Todo()).getItems();
+
+
+class Person {
+    
+
+    getAge() {
+
+    }
+}
